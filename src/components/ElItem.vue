a20b7f59df17c92df7f04d247dd7c22e6426c859
Roman Fomichev <romanfomic@gmail.com>
2024-04-05 09:22:51 +0300

    Add basic elevator structure

---
+++ src/components/ElItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="el-system">
+        <div class="elevator"></div>
+        <div class="shaft" v-for="position in floors">
+            <div class="floor">
+                <div class="floor-shaft"></div>
+                <div class="position">{{ floors - position + 1 }}</div>
+                <button class="el-btn">вызвать</button>
+            </div>
+            <hr/>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                currentPos: 1,
+                isActive: false,
+                isResting: false,
+                floors: 5
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>

99ca7228a72691da0a10973edb1036b53912a013
Roman Fomichev <romanfomic@gmail.com>
2024-04-05 09:34:24 +0300

    Add styles for elevator

--- src/components/ElItem.vue
+++ src/components/ElItem.vue
@@ -1,11 +1,10 @@
 <template>
-    <div class="el-system">
-        <div class="elevator"></div>
+    <div>
         <div class="shaft" v-for="position in floors">
             <div class="floor">
-                <div class="floor-shaft"></div>
+                <div class="floor-shaft" :class="{ 'current-floor': currentPos === floors + 1 - position }"></div>
                 <div class="position">{{ floors - position + 1 }}</div>
-                <button class="el-btn">вызвать</button>
+                <button class="el-btn"></button>
             </div>
             <hr/>
         </div>
@@ -19,11 +18,47 @@
                 currentPos: 1,
                 isActive: false,
                 isResting: false,
-                floors: 5
+                floors: 5,
+                elQuery: []
             }
         }
     }
 </script>
 
 <style scoped>
+    .shaft {
+        height: 50px;
+    }
+    .floor-shaft {
+        height: 50px;
+        width: 40px;
+        border-right: 1px solid black;
+        border-left: 1px solid black;
+        margin: 0;
+        margin-left: 10px;
+    }
+    hr {
+        margin: 0;
+    }
+    .floor {
+        display: flex;
+        align-items: center;
+    }
+    .position {
+        margin-left: 10px;
+    }
+    .el-btn {
+        margin-left: 5px;
+        padding: 0;
+        height: 16px;
+        width: 16px;
+        border-radius: 8px;
+        border: 1px solid deepskyblue;
+    }
+    .el-btn:hover {
+        cursor: pointer;
+    }
+    .current-floor {
+        background-color: dodgerblue;
+    }
 </style>
